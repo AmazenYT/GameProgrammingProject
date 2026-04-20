0c1f16cd5fb0d2ba19368def520a4c64ffc022ea
AmazenYT <frankiecahill90@gmail.com>
2026-04-20 20:20:39 +0100

    shader materials

---
+++ My project/Assets/Scripts/RingScript.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class RingScript : MonoBehaviour
+{
+    public Vector3 rotationSpeed = new Vector3(0f, 180f, 0f);
+   
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(rotationSpeed * Time.deltaTime);
+    }
+
+
+}
